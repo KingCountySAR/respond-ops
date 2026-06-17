8942c905dc83394c5e45d22ad9045aaced691977
Matt Cosand <matt@cosand.net>
2026-06-16 22:59:40 -0700

    Display websocket connect status in toolbar

--- web/src/components/ToolbarPage.tsx
+++ web/src/components/ToolbarPage.tsx
@@ -1,5 +1,6 @@
 import CloudIcon from '@mui/icons-material/Cloud'
 import NoCloudIcon from '@mui/icons-material/CloudOff'
+import ConnectingIcon from '@mui/icons-material/CloudSync'
 import { Breakpoint, Container, Stack, styled } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
@@ -10,6 +11,7 @@ import { AppMenu } from '@web/components/AppMenu'
 import { LoginPanel } from '@web/components/LoginPanel'
 import { useAuthContext } from '@web/lib/authProvider'
 import { useConfigContext } from '@web/lib/configProvider'
+import { useSocketContext } from '@web/store/websocketStore'
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 import { Link } from 'react-router'
@@ -17,8 +19,25 @@ import { Link } from 'react-router'
 
 const Main = styled('main')({})
 
+const ConnectIconLookup = {
+  idle: NoCloudIcon,
+  connecting: ConnectingIcon,
+  connected: CloudIcon,
+}
+
+const OnlineStatus = observer(() => {
+  const sockets = useSocketContext()
+  const Icon = ConnectIconLookup[sockets.connectState]
+
+  return (
+    <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+      <Typography variant="caption">{sockets.reconnectCountdownText}</Typography>
+      <Icon fontSize="medium" />
+    </Stack>
+  )
+})
+
 export const ToolbarPage = observer(({ children, maxWidth }: { children: React.ReactNode; maxWidth?: false | Breakpoint }) => {
-  const connected = true
   const config = useConfigContext()
   const auth = useAuthContext()
 
@@ -45,14 +64,14 @@ export const ToolbarPage = observer(({ children, maxWidth }: { children: React.R
                 alignItems: 'center',
               }}
             >
-              {connected ? <CloudIcon fontSize="medium" /> : <NoCloudIcon fontSize="medium" />}
+              <OnlineStatus />
             </Stack>
           </Typography>
           <AppMenu />
         </Toolbar>
       </AppBar>
       <Box className="toolbar-filler" sx={{ height: { xs: 56, sm: 64 } }} />
-      <Main sx={{ py: 2, flex:'1 1 auto', display:'flex', flexDirection:'column' }}>
+      <Main sx={{ py: 2, flex: '1 1 auto', display: 'flex', flexDirection: 'column' }}>
         {children}
       </Main>
     </Container>
